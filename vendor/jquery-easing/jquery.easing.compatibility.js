3f88d93c3ad7b7f7ed0c6a457e92fc26b6dbc43c
huaj7 <36248061+huaj7@users.noreply.github.com>
2018-08-27 18:33:57 -0700

    Revert

--- vendor/jquery-easing/jquery.easing.compatibility.js
+++ vendor/jquery-easing/jquery.easing.compatibility.js
@@ -1,3 +1,13 @@
+/*
+ * Easing Compatibility v1 - http://gsgd.co.uk/sandbox/jquery/easing
+ *
+ * Adds compatibility for applications that use the pre 1.2 easing names
+ *
+ * Copyright (c) 2007 George Smith
+ * Licensed under the MIT License:
+ *   http://www.opensource.org/licenses/mit-license.php
+ */
+
 (function($){
 $.extend( $.easing,
 {
